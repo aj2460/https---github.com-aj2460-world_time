8efd412e8382746f2a208b53dc23bd1e10bcc8ae
Anand <ajkadakkal@gmail.com>
2021-11-18 18:15:14 +0530

    show date time in loading widget

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:worldtime/services/world_time.dart';
+
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
 
@@ -8,8 +10,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String timeString = "Loading";
+  void setUpWorldTime() async {
+    WorldTime wtInstance =
+        WorldTime(location: "India", flag: "india.jpg", url: "Asia/Kolkata");
+    await wtInstance.getTime();
+    setState(() {
+      timeString = wtInstance.time;
+    });
+  }
+
+  void initState() {
+    super.initState();
+    setUpWorldTime();
+    //print("msg after receivin time data");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(timeString),
+      ),
+    );
   }
 }
